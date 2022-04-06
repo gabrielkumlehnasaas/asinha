b634ae36d0e6440cef4cc2e13fd8b72b7acdd13d
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-04-06 11:34:25 -0300

    Cria classe Customer e views MVC

---
+++ grails-app/domain/com/asinha/domain/Customer.groovy
@@ -0,0 +1,20 @@
+package com.asinha.domain
+
+class Customer {
+
+    String nomeCompanhia
+    String cpfCnpj
+    String email
+    String telefone
+    String cep
+    String estado
+    String cidade
+    String bairro
+    String endereco
+    String numero
+    String complemento
+    Date dataCriacao
+
+    static constraints = {
+    }
+}
