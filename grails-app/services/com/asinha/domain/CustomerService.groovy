a9ca1415d2786485641100bbec1ed973922cff98
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-04-18 09:52:55 -0300

    refatora cutomer service: - adiciona função save

--- grails-app/services/com/asinha/domain/CustomerService.groovy
+++ grails-app/services/com/asinha/domain/CustomerService.groovy
@@ -1,18 +1,24 @@
 package com.asinha.domain
 
-import grails.gorm.services.Service
+import com.asinha.domain.Customer
 
-@Service(Customer)
-interface CustomerService {
+class CustomerService {
 
-    Customer get(Serializable id)
-
-    List<Customer> list(Map args)
-
-    Long count()
-
-    void delete(Serializable id)
-
-    Customer save(Customer customer)
-
+    def save(Map params) {
+        Customer customer = new Customer()
+        customer.address = params.address
+        customer.addressNumber = params.addressNumber
+        customer.city = params.city
+        customer.complement = params.complement
+        customer.cpfCnpj = params.cpfCnpj
+        customer.deleted = 0
+        customer.email = params.email
+        customer.phone = params.phone
+        customer.name = params.name
+        customer.postalCode = params.postalCode
+        customer.province = params.province
+        customer.state = params.state
+        customer.save(failOnError: true)
+        return customer
+    }
 }
