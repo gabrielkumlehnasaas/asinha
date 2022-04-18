394525f6b66dc2715c13604a5f7fcfa831c82aa3
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-04-18 09:57:44 -0300

    refatora dominio customer

--- grails-app/domain/com/asinha/domain/Customer.groovy
+++ grails-app/domain/com/asinha/domain/Customer.groovy
@@ -2,19 +2,31 @@ package com.asinha.domain
 
 class Customer {
 
-    String nomeCompanhia
+    String address
+    String addressNumber
+    String city
+    String complement
     String cpfCnpj
+    Integer deleted
     String email
-    String telefone
-    String cep
-    String estado
-    String cidade
-    String bairro
-    String endereco
-    String numero
-    String complemento
-    Date dataCriacao
+    String phone
+    String name
+    String postalCode
+    String province
+    String state
 
     static constraints = {
+        address size: 1..255, nullable: false
+        addressNumber size: 1..255, nullable: false
+        city size: 1..255, nullable: false
+        complement size: 1..255
+        cpfCnpj size: 11..14, nullable: false
+        deleted min: 0, max: 1, nullable: false
+        email email: true, size: 5..255, nullable: false
+        phone size: 1..255, nullable: false
+        name size: 1..255, nullable: false
+        postalCode size: 1..255, nullable: false
+        province size: 1..255, nullable: false
+        state size: 1..255, nullable: false
     }
 }
