2f51a70af2f1cd7001f59effa7c16b2045d424d3
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-04-28 10:46:02 -0300

    merge de funções de customer-create.js

--- grails-app/assets/javascripts/asinha/customer-create.js
+++ grails-app/assets/javascripts/asinha/customer-create.js
@@ -75,3 +75,19 @@ emailInputReference.addEventListener("focusout", function() {
         errorMessageReference.innerHTML = "";
     }
 })
+
+$(document).ready(function () {
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        let infosCustomer = {};
+        let data = new FormData(document.querySelector("form"));
+        data.forEach(function (value,key) {
+            if (key != "cpfCnpjRadio") infosCustomer[key] = value;
+        });
+        var url = document.querySelector("form").getAttribute("action");
+        $.post(url, infosCustomer, function(response) {
+            console.log(response);
+            window.location.href = document.querySelector("form").getAttribute("data-redirect")
+        });
+    });
+});
