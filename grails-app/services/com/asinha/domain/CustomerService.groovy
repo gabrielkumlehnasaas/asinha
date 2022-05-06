ad4d2376ec7310362b02e46abaa61f6c8d1fccef
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-06 11:18:21 -0300

    atualiza metodo de save para explicitar parametros

--- grails-app/services/com/asinha/domain/CustomerService.groovy
+++ grails-app/services/com/asinha/domain/CustomerService.groovy
@@ -9,7 +9,18 @@ class CustomerService {
     }
 
     def save(Map params) {
-        Customer customer = new Customer(params)
+        Customer customer = new Customer()
+        customer.address = params.address
+        customer.addressNumber = params.addressNumber
+        customer.city = params.city
+        customer.complement = params.complement
+        customer.cpfCnpj = params.cpfCnpj
+        customer.email = params.email
+        customer.phone = params.phone
+        customer.name = params.name
+        customer.postalCode = params.postalCode
+        customer.province = params.province
+        customer.state = params.state
         customer.save(failOnError: true)
         return customer
     }
