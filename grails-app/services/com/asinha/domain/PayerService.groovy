11d4451c54fcdd031d09f08f86de18b2a95334b9
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-06 11:22:42 -0300

    corrige metodo save para explicitar paramentros

--- grails-app/services/com/asinha/domain/PayerService.groovy
+++ grails-app/services/com/asinha/domain/PayerService.groovy
@@ -11,7 +11,20 @@ class PayerService {
     }
 
     def save(Map params) {
-        Payer payer = new Payer(params)
+        Payer payer = new Payer()
+        payer.address = params.address
+        payer.addressNumber = params.addressNumber
+        payer.city = params.city
+        payer.complement = params.complement
+        payer.cpfCnpj = params.cpfCnpj
+        payer.deleted = 0
+        payer.email = params.email
+        payer.phone = params.phone
+        payer.name = params.name
+        payer.postalCode = params.postalCode
+        payer.province = params.province
+        payer.state = params.state
+        payer.customer = Customer.get(params.int("customer"))
         payer.save(failOnError: true)
         return payer
     }
