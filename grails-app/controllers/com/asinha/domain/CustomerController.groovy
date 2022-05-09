2e07daa9d90990aca87260d4d7b1f5e256c57d1f
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-09 14:49:57 -0300

    realiza update a partir de branch anterior

--- grails-app/controllers/com/asinha/domain/CustomerController.groovy
+++ grails-app/controllers/com/asinha/domain/CustomerController.groovy
@@ -1,41 +1,49 @@
 package com.asinha.domain
+
+import com.asinha.base.BaseController
 import com.asinha.domain.Customer
+
+import grails.converters.JSON
 import grails.validation.ValidationException
 import static org.springframework.http.HttpStatus.*
-import grails.converters.JSON
 
-class CustomerController {
+class CustomerController extends BaseController {
 
     def customerService
 
-    def list() {
-        return [customerList: customerService.list()]
+    def index() {}
+
+    def create() {}
+    
+    def save() {
+        try {
+            Customer customer = customerService.save(params)
+            if(customer) render ([success: true] as JSON)
+        } catch(Exception exception) {
+            render([success: false, message: "Erro, tente novamente"] as JSON)
+        }
     }
 
-    def create() {
+    def list() {
+        return [customerList: Customer.list(max: getLimitPerPage(), offset: getCurrentPage()), totalCount: Customer.count()]
     }
+
+
 
     def show() {
         Integer id = params.int("id")
         return [customer: customerService.getCustomer(id)]
     }
 
-        def update() {
-            try {
-                Customer customer = customerService.update(params)
-                if (customer){
-                    redirect([action: "show", id: customer.id])
-                }
-            } catch(Exception exception) {}
-        }
+    
 
-    def save() {
+    def update() {
         try {
-            Customer customer = customerService.save(params)
-            if(customer) {
-                redirect(action: "list")
+            Customer customer = customerService.update(params)
+            if (customer){
+                redirect([action: "show", id: customer.id])
             }
-        } catch(Exception exception) {
-        }
+        } catch(Exception exception) {}
     }
+
 }
