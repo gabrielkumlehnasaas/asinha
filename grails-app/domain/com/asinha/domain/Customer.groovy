fb9045f4c3e93c041ef60741372fc2209ef34e06
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-12 16:11:54 -0300

    corrige espaços

--- grails-app/domain/com/asinha/domain/Customer.groovy
+++ grails-app/domain/com/asinha/domain/Customer.groovy
@@ -5,25 +5,25 @@ import com.asinha.base.BaseEntity
 class Customer extends BaseEntity{
 
     String address
-    
+
     String addressNumber
-    
+
     String city
-    
+
     String complement
-    
+
     String cpfCnpj
-    
+
     String email
-    
+
     String phone
-    
+
     String name
-    
+
     String postalCode
-    
+
     String province
-    
+
     String state
 
     static constraints = {
