3a470d9d2d8d531969c065208b50b08229a391a5
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-12 16:15:45 -0300

    corrige espacos

--- src/main/groovy/com/asinha/base/BaseEntity.groovy
+++ src/main/groovy/com/asinha/base/BaseEntity.groovy
@@ -3,11 +3,11 @@ package com.asinha.base
 abstract class BaseEntity {
 
     Boolean deleted = false
-    
+
     Date dateCreated = new Date()
-    
+
     Date lastUpdate = dateCreated
-    
+
     Date dateDeleted
 
     static mapping = {
