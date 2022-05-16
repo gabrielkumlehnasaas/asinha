0e7571b3f7e1212692465386388d0d087a8fa7e3
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-16 13:42:02 -0300

    troca para earlyReturn

--- grails-app/assets/javascripts/asinha/payer-create.js
+++ grails-app/assets/javascripts/asinha/payer-create.js
@@ -36,13 +36,14 @@ function PayerCreate() {
     
     var validateCep = function(data) {
         let error = ("erro" in data);
-        if (!error) {
-            errorMessageReference.innerHTML = "";
-            fillForm(data);
-        } else {
+        if (error) {
             cleanForm();
             errorMessageReference.innerHTML = "CEP inválido";
+            return
         };
+        
+        errorMessageReference.innerHTML = "";
+        fillForm(data);
     };
 
     var bindCep = function() {
