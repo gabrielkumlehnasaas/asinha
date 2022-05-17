538166344f5cef6987c75ce761274f403d88923f
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-17 10:46:01 -0300

    remove import desnecessario

--- grails-app/jobs/criaapp/TaskDueDateJob.groovy
+++ grails-app/jobs/criaapp/TaskDueDateJob.groovy
@@ -2,14 +2,11 @@ package criaapp
 
 import com.asinha.domain.Payment
 import com.asinha.enums.PaymentStatus
-import com.asinha.domain.PaymentService
 
 import grails.gorm.transactions.Transactional
 
 @Transactional
 class TaskDueDateJob {
-
-  def paymentService
   
   static triggers = {
     cron name: 'dueDate', cronExpression: "0 0/1 * 1/1 * ?"
@@ -27,6 +24,5 @@ class TaskDueDateJob {
         payment.save(flush: true, failOnError:true)
       }
     }
-  } 
-  
+  }
 }
