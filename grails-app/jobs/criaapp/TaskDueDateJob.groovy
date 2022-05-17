6494d6e4721336e039f77976877c27ccebcb2818
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-17 10:45:06 -0300

    finaliza job de mudança de status

--- grails-app/jobs/criaapp/TaskDueDateJob.groovy
+++ grails-app/jobs/criaapp/TaskDueDateJob.groovy
@@ -4,6 +4,9 @@ import com.asinha.domain.Payment
 import com.asinha.enums.PaymentStatus
 import com.asinha.domain.PaymentService
 
+import grails.gorm.transactions.Transactional
+
+@Transactional
 class TaskDueDateJob {
 
   def paymentService
@@ -16,16 +19,14 @@ class TaskDueDateJob {
 
   def execute(){
     Date today = new Date()
-    println(today)
-    List<payment> paymentList = Payment.list()
-    paymentList.forEach(payment -> {
-      println(payment.dueDate)
+    List<Payment> paymentList = Payment.list()
+    for(Payment payment : paymentList) {
       if(today.after(payment.dueDate)) {
         payment.status = PaymentStatus.OVERDUE
         payment.lastUpdate = today
         payment.save(flush: true, failOnError:true)
       }
-    })
+    }
   } 
   
 }
