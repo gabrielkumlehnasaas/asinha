24e030e878757ee38fc886b602254a3dd38c5b1f
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-17 10:11:32 -0300

    refatorando

--- grails-app/jobs/criaapp/TaskDueDateJob.groovy
+++ grails-app/jobs/criaapp/TaskDueDateJob.groovy
@@ -1,13 +1,31 @@
 package criaapp
 
+import com.asinha.domain.Payment
+import com.asinha.enums.PaymentStatus
+import com.asinha.domain.PaymentService
+
 class TaskDueDateJob {
+
+  def paymentService
   
   static triggers = {
     cron name: 'dueDate', cronExpression: "0 0/1 * 1/1 * ?"
   }
   
   static concurrent = false
-    def execute(){
-    print "Job run!"
-  }
+
+  def execute(){
+    Date today = new Date()
+    println(today)
+    List<payment> paymentList = Payment.list()
+    paymentList.forEach(payment -> {
+      println(payment.dueDate)
+      if(today.after(payment.dueDate)) {
+        payment.status = PaymentStatus.OVERDUE
+        payment.lastUpdate = today
+        payment.save(flush: true, failOnError:true)
+      }
+    })
+  } 
+  
 }
