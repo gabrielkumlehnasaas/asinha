3287945070e58ff30c337ad1f88c9ea16e07f24c
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-18 11:16:25 -0300

    REFATORANDO

--- grails-app/services/com/asinha/domain/CustomerService.groovy
+++ grails-app/services/com/asinha/domain/CustomerService.groovy
@@ -1,6 +1,8 @@
 package com.asinha.domain
 
 import com.asinha.domain.Customer
+import com.asinha.utils.DomainUtils
+import com.asinha.utils.ValidationUtils
 
 import grails.gorm.transactions.Transactional
 
@@ -39,6 +41,22 @@ class CustomerService {
         customer.province = params.province
         customer.state = params.state
         customer.save(failOnError: true)
+        return customer
+    }
+
+    public Customer validate(Customer customer, Map params) {
+        if(!ValidationUtils.addressNumberValidate(params.addressNumber)) {
+            DomainUtils.addError(customer, "Endereço sem número"))
+        }
+        if (!ValidationUtils.phoneValidate(params.phone)) {
+            DomainUtils.addError(customer, "Número de telefone inválido")
+        }
+        if (!ValidationUtils.cpfCnpjValidate(params.cpfCnpj)) {
+            DomainUtils.addError(customer, "CPF/CNPJ inválido")
+        }
+        if (!ValidationUtils.postalCodeValidate(params.postalCode)) {
+            DomainUtils.addError(customer, "CEP inválido")
+        }
         return customer
     }
 }
