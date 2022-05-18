9ca0b2046846f1f6c78938b6c96a796f1beb31ff
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-18 11:18:14 -0300

    extends basecontroller on payment

--- grails-app/controllers/com/asinha/domain/PaymentController.groovy
+++ grails-app/controllers/com/asinha/domain/PaymentController.groovy
@@ -1,5 +1,6 @@
 package com.asinha.domain
 
+import com.asinha.base.BaseController
 import com.asinha.domain.Customer
 import com.asinha.domain.Payer
 import com.asinha.domain.Payment
@@ -7,7 +8,7 @@ import com.asinha.enums.PaymentMethod
 
 import grails.converters.JSON
 
-class PaymentController {
+class PaymentController extends BaseController{
 
     def payerService
     def paymentService
