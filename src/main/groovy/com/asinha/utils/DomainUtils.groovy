05cea60b4cf19f798bbababcc1540e6a852118aa
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-18 11:10:20 -0300

    remove espaco

--- src/main/groovy/com/asinha/utils/DomainUtils.groovy
+++ src/main/groovy/com/asinha/utils/DomainUtils.groovy
@@ -2,7 +2,6 @@ package com.asinha.utils
 
 class DomainUtils {
 
-
     public static Object addError(Object entity, String message) {
         entity.errors.reject("", null, message)
         return entity
