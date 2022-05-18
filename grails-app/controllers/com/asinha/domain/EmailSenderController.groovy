832126314eaaa80f349623fe5837e8e5dcb5edb7
Hebert Barbosa <hebert.barbosa@asaas.com.br>
2022-05-18 10:06:25 -0300

    adicionando flash.type para estilização de mensagem de error e success

--- grails-app/controllers/com/asinha/domain/EmailSenderController.groovy
+++ grails-app/controllers/com/asinha/domain/EmailSenderController.groovy
@@ -5,13 +5,20 @@ class EmailSenderController {
     def index() {}
     
     def send() {
-    sendMail {
-        to params.emailImputSendEmail
-        subject params.subjectImputSendEmail
-        text params.messageImputSendEmail
-    }
+        try {
+            sendMail {
+                to params.emailSendEmail
+                subject params.subjectSendEmail
+                text params.messageSendEmail
+            }
+                flash.message = "E-mail enviado em "+new Date()
+                flash.type = "success"
+        } catch (Exception e) {
+            flash.message = "Erro ao enviar E-mail"
+            flash.type = "error"
+        } finally {
+            redirect action:"index"
+        }
 
-        flash.message = "E-mail enviado em "+new Date()
-        redirect action:"index"
     }
 }
