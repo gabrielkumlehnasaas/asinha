3db04c2f45851c544da51daf569d4074b77a398d
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-05-21 10:35:07 -0300

    payment validation

--- grails-app/services/com/asinha/domain/PaymentService.groovy
+++ grails-app/services/com/asinha/domain/PaymentService.groovy
@@ -6,6 +6,8 @@ import com.asinha.domain.Payment
 import com.asinha.enums.PaymentMethod
 import com.asinha.enums.PaymentStatus
 import com.asinha.utils.CustomDateUtils
+import com.asinha.utils.DomainUtils
+import com.asinha.utils.ValidationUtils
 
 import grails.gorm.transactions.Transactional
 import java.math.BigDecimal
@@ -22,7 +24,9 @@ class PaymentService {
 
     public Payment save(Map params) {
         Payment payment = new Payment()
-        payment.value = new BigDecimal(params.value)
+        payment = validate(payment, params)
+        if (payment.hasErrors()) return payment
+        payment.value = new BigDecimal(params.value.replaceAll(",", ""))
         payment.description = params.description
         payment.method = PaymentMethod.valueOf(params.method) 
         payment.status = PaymentStatus.PENDING
@@ -37,8 +41,23 @@ class PaymentService {
         Payment payment = Payment.get(paymentId)
         payment.status = PaymentStatus.PAID
         payment.paymentDate = new Date()
-        payment.lastUpdate = new Date()
         payment.save(flush: true, failOnError:true)
+        return payment
+    }
+
+    public Payment validate(Payment payment, Map params) {
+        if (!ValidationUtils.validateValue(params.value)) {
+            DomainUtils.addError(payment, 'Cobrança mínima de R$5.00')
+        }
+        if (!(ValidationUtils.validateNotNull(params.description))) {
+            DomainUtils.addError(payment, "Campo Cidade é obrigatório")
+        }
+        if (!ValidationUtils.validateMethod(params.method)) {
+            DomainUtils.addError(payment, "Método de pagamento inválido")
+        }
+        if (!ValidationUtils.validateDueDate(params.dueDate)) {
+            DomainUtils.addError(payment, "Data de vencimento inválida")
+        }
         return payment
     }
 }
