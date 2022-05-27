42798fcebbba3a001bfe990fbb39061e961ea6e7
Hebert Barbosa <hebert.barbosa@asaas.com.br>
2022-05-27 14:11:51 -0300

    ajusta bootstrap

--- grails-app/init/criaapp/BootStrap.groovy
+++ grails-app/init/criaapp/BootStrap.groovy
@@ -12,7 +12,5 @@ class BootStrap {
         new Role(authority: 'ROLE_ADMIN').save()
         new Role(authority: 'ROLE_USER').save()
     }
-    def destroy = {
-        cleanUp()
-    }
+    def destroy = {}
 }
