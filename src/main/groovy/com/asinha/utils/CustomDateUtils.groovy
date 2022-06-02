37116c6bd52f16029f1be1ac1b1f35aa1284e1e4
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-06-02 09:42:29 -0300

    remove barra

--- src/main/groovy/com/asinha/utils/CustomDateUtils.groovy
+++ src/main/groovy/com/asinha/utils/CustomDateUtils.groovy
@@ -29,7 +29,7 @@ class CustomDateUtils {
         calendar.set(Calendar.HOUR_OF_DAY, 0)
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
-        calendar.set(Calendar.MILLISECOND, 0)\
+        calendar.set(Calendar.MILLISECOND, 0)
         return calendar.getTime()
     }
 
