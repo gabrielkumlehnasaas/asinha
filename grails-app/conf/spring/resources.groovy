434c3466dda8e381c90ebb2b54e25b9e7ce63b0d
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2022-06-10 14:00:17 -0300

    add espaco

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,6 +1,6 @@
 import com.asinha.domain.CustomUserDetailsService
 import com.asinha.utils.UserPasswordEncoderListener
-// Place your Spring DSL code here
+
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
     userDetailsService(CustomUserDetailsService)
